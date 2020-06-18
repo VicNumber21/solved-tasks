18d505b51f100e86b2ade3a4a20f6b840d99f067
Victor Portnov <vic.portnov@gmail.com>
2020-06-18 15:50:09 +0300

    Balanced forest solution. A bit better than previous but still fails with wrong answer and also by timeout.

--- tasks/balanced-forest/solution.js
+++ tasks/balanced-forest/solution.js
@@ -3,35 +3,50 @@ export { balancedForest as solution };
 function balancedForest(vertices, edges) {
   let tree = buildTree(vertices, edges);
   const overallSum = calculateSumsAndAssignIds(tree);
+  let branchSums = buildBranchSums(tree);
   const lowerBound = overallSum / 3n + 1n;
   const upperBound = overallSum / 2n;
   tree.sort((v1, v2) => Number(v1.sum - v2.sum));
-  let index = leftBinarySearch(tree, lowerBound);
+  branchSums.sort((v1, v2) => Number(v1.sum - v2.sum));
+  let leftIndex = leftBinarySearch(tree, lowerBound);
+  let rightIndex = tree.length - 2;
   let third = false;
 
-  while(!third && tree[index].sum <= upperBound) {
-    const currentSum = tree[index].sum;
+  while(!third && (tree[leftIndex].sum <= upperBound || rightIndex > leftIndex && tree[rightIndex].sum > upperBound)) {
+    const leftThird = tree[leftIndex].sum;
+    const rightSum = tree[rightIndex].sum;
+    const rightThird = overallSum - rightSum;
 
-    if (foundInDifferentBranches(tree, index) ||
-        foundInSameBranch(tree, index) ||
-        reminderFound(tree, index, overallSum - 2n * currentSum))
-    {
-      third = currentSum;
+    if (lowerBound < rightThird && rightThird < leftThird) {
+      if (searchReminderInBranch(tree, rightIndex, rightSum - rightThird)) {
+        third = rightThird;
+      }
+
+      --rightIndex;
     }
+    else {
+      if (searchDifferentBranches(tree, leftIndex) ||
+        searchInSameBranch(tree, leftIndex) ||
+        searchReminderOutOfBranch(tree, leftIndex, overallSum - 2n * leftThird) ||
+        searchInBranchSums(branchSums, tree[leftIndex]))
+      {
+        third = leftThird;
+      }
 
-    while (currentSum === tree[index].sum) {
-      ++index;
+      while (leftThird === tree[leftIndex].sum) {
+        ++leftIndex;
+      }
     }
   }
 
   return third ? 3n * third - overallSum : -1;
 }
 
-function foundInDifferentBranches(tree, index) {
+function searchDifferentBranches(tree, index) {
   return tree[index].sum === tree[index + 1].sum;
 }
 
-function foundInSameBranch(tree, index) {
+function searchInSameBranch(tree, index) {
   const doubledThird = 2n * tree[index].sum;
   let doubledThirdIndex = leftBinarySearch(tree, doubledThird);
   let found = false;
@@ -44,13 +59,37 @@ function foundInSameBranch(tree, index) {
   return found;
 }
 
-function reminderFound(tree, index, reminder) {
+function searchReminderOutOfBranch(tree, index, reminder) {
   let reminderIndex = leftBinarySearch(tree, reminder);
   let found = false;
 
   while (!found && tree[reminderIndex].sum === reminder) {
     found = !isSubset(tree[index], tree[reminderIndex]);
     ++reminderIndex;
+  }
+
+  return found;
+}
+
+function searchReminderInBranch(tree, index, reminder) {
+  let reminderIndex = leftBinarySearch(tree, reminder);
+  let found = false;
+
+  while (!found && tree[reminderIndex].sum === reminder) {
+    found = isSubset(tree[index], tree[reminderIndex]);
+    ++reminderIndex;
+  }
+
+  return found;
+}
+
+function searchInBranchSums(branchSums, vertex) {
+  let sumIndex = leftBinarySearch(branchSums, vertex.sum);
+  let found = false;
+
+  while (!found && branchSums[sumIndex].sum === vertex.sum) {
+    found = (branchSums[sumIndex].vertex !== vertex);
+    ++sumIndex;
   }
 
   return found;
@@ -69,8 +108,10 @@ function buildTree(vertices, edges) {
   });
 
   for(let edge of edges) {
-    let vertex = tree[edge[0] - 1];
-    vertex.next.push(tree[edge[1] - 1]);
+    let vertex1 = tree[edge[0] - 1];
+    let vertex2 = tree[edge[1] - 1];
+    vertex1.next.push(vertex2);
+    vertex2.next.push(vertex1);
   }
 
   return tree;
@@ -79,6 +120,7 @@ function buildTree(vertices, edges) {
 function calculateSumsAndAssignIds(tree) {
   let root = tree[0];
   root.id = '0';
+  root.aSum = root.value;
   let vertexStack = [root];
   let nextStack = [root.next]
 
@@ -91,7 +133,9 @@ function calculateSumsAndAssignIds(tree) {
 
       for (let [index, vertex] of next.entries()) {
         vertex.id = `${parent.id}-${index}`;
+        vertex.aSum = parent.aSum + vertex.value;
         vertexStack.push(vertex);
+        vertex.next = vertex.next.filter(v => v !== parent);
         nextStack.push(vertex.next);
       }
     }
@@ -102,6 +146,42 @@ function calculateSumsAndAssignIds(tree) {
   }
 
   return root.sum;
+}
+
+function buildBranchSums(tree) {
+  let root = tree[0];
+  let overallSum = root.sum;
+  const rootBranchSums = root.next.map(v => {
+    return {
+      sum: v.sum + root.value,
+      vertex: root
+    };
+  });
+  let branchSums = [rootBranchSums];
+  let vertexStack = [root];
+
+  while (vertexStack.length > 0) {
+    const parent = vertexStack.pop();
+
+    for(let vertex of parent.next) {
+      let vertexBranchSums = vertex.next.map(v => {
+        return {
+          sum: v.sum + vertex.value,
+          vertex: vertex
+        };
+      });
+
+      vertexBranchSums.push({
+        sum: overallSum - vertex.sum + vertex.value,
+        vertex: vertex
+      });
+
+      vertexStack.push(vertex);
+      branchSums.push(vertexBranchSums);
+    }
+  }
+
+  return branchSums.reduce((acc, val) => acc.concat(val), []);
 }
 
 function leftBinarySearch(sortedArray, value) {
