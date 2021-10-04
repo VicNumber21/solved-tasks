0050638ae41ffa880a95d78f13035af06ecc5f2e
Victor Portnov <vic.portnov@gmail.com>
2021-10-04 13:24:33 +0300

    Array Manipulation reworked to use ES6 Map instead of Object. Code gets a bit leaner and a bit faster (probably)

--- tasks/array-manipulation/solution.js
+++ tasks/array-manipulation/solution.js
@@ -2,26 +2,27 @@ export { fn as solution };
 
 function fn(n, queries) {
   let remainingSum = 0;
-  const lefts = {};
-  const rights = {};
+  const lefts = new Map();
+  const rights = new Map();
 
-  for (const [left, right, value] of queries) {
+  for (let [left, right, value] of queries) {
+    left = +left;
+    right = +right;
     remainingSum += value;
-    lefts[left] = (lefts[left] || 0) + value;
-    rights[right] = (rights[right] || 0) + value;
+    lefts.set(left, (lefts.get(left) || 0) + value);
+    rights.set(right, (rights.get(right) || 0) + value);
   }
 
-  const sortedLefts = Object.entries(lefts).sort(([left1], [left2]) => left2 - left1);
-  const sortedRights = Object.entries(rights).sort(([right1], [right2]) => right2 - right1);
+  const sortedLefts = Array.from(lefts).sort(([left1], [left2]) => left2 - left1);
+  const sortedRights = Array.from(rights).sort(([right1], [right2]) => right2 - right1);
 
   let max = 0;
   let currentSum = 0;
   while (sortedLefts.length > 0 && ((max - currentSum) < remainingSum)) {
     let [left, increment] = sortedLefts.pop();
-    left = +left;
 
     let decrement = 0;
-    while (sortedRights.length > 0 && (+sortedRights[sortedRights.length - 1][0]) < left) {
+    while (sortedRights.length > 0 && sortedRights[sortedRights.length - 1][0] < left) {
       const [, value] = sortedRights.pop();
       decrement += value;
     }
